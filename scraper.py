1f88e58ab12b4d6e2747cd2771a2ca009ee6597f
Yallow <tylerbouma1@gmail.com>
2019-04-28 11:53:14 -0500

    first commit

---
+++ scraper.py
@@ -0,0 +1,19 @@
+import wikipedia
+import requests
+import json
+import datetime
+
+# use the wikipedia library to easily interact with wikipedia pages
+
+# each link will be stored in it's own class
+class 
+
+# lets start by statically setting our starter page to 'python'
+start_page = wikipedia.page("Python")
+
+# get all of the links on the start page
+for link in start_page.links:
+    # store each of the links in an object
+    
+    link.content
+    
