8cbadf4ad585d118600f9a584677b0d215064aa5
Yallow <tylerbouma1@gmail.com>
2019-04-28 15:16:09 -0500

    gets the first five links of the first five links of a user requested page

--- scraper.py
+++ scraper.py
@@ -1,19 +1,37 @@
-import wikipedia
+import wikipediaapi
 import requests
 import json
 import datetime
 
+
+def get_links(page):
+    links = page.links
+    # return the first five links from the page
+    link_arr = []
+    link_wiki = wikipediaapi.Wikipedia('en')
+    for i, link in enumerate(links):
+        if i > 4:
+            break
+        link_page = link_wiki.page(link)
+        link_arr.append(link_page)
+
+    return link_arr
+
+
 # use the wikipedia library to easily interact with wikipedia pages
+wiki_wiki = wikipediaapi.Wikipedia('en')
 
-# each link will be stored in it's own class
-class 
+input_page = input("Page to search: ")
+# lets start by statically setting our starter page to 'dog'
+start_page = wiki_wiki.page(input_page)
+if start_page.exists() != True:
+    print("Page: %s does not exist" % input_page)
+    exit
 
-# lets start by statically setting our starter page to 'python'
-start_page = wikipedia.page("Python")
+link_pages = get_links(start_page)
+# now get the first five links off of the links pages
+for link_page in link_pages:
+    sub_link_page = get_links(link_page)
+    print(sub_link_page)
 
-# get all of the links on the start page
-for link in start_page.links:
-    # store each of the links in an object
-    
-    link.content
     
