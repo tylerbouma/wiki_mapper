608a65ce7c1dbdb0c52e555f84e70a12124ea559
Yallow <tylerbouma1@gmail.com>
2019-04-30 22:05:39 -0500

    rearranged structure to use a class

--- scraper.py
+++ scraper.py
@@ -8,56 +8,54 @@ import os
 
 from IPython.display import Image, display
 
+class Graph:
+    def __init__(self, search_term, how_deep=0):
+        # initialize the graph
+        self.g = pydot.Dot(graph_type="digraph")
+        self.wiki_wiki = wikipediaapi.Wikipedia('en')
+        self.start_page = self.wiki_wiki.page(search_term)
+        self.search_term = search_term
+        self.how_deep = how_deep
 
-def get_links(page):
-    links = page.links
-    # return the first five links from the page
-    link_arr = []
-    link_wiki = wikipediaapi.Wikipedia('en')
-    for i, link in enumerate(links):
-        if i > 4:
-            break
-        link_page = link_wiki.page(link)
-        link_arr.append(link_page)
+        # check if the starter page exists
+        if self.start_page.exists() != True:
+            print("Page: %s does not exist" % search_term)
+            exit
 
-    return link_arr
+    def create_links(self):
+        #TODO: Try doing this recursively
+        page = self.start_page
+        for i in range(self.how_deep):
+            print("We are %s levels deep." % i)
+            # down the rabbit hole we go
+            links = page.links
+            for i, link in enumerate(links):
+                if i > 8:
+                    break
+                link_page = self.wiki_wiki.page(link)
+                self.create_node(link_page)
+                self.create_edge(page, link_page)
+            page = link_page
+
+    def create_node(self, sub_page=None):
+        node = pydot.Node(sub_page.title, style="filled", fillcolor="red", fontname='Courier', fontsize='10')
+        self.g.add_node(node)
+
+    def create_edge(self, parent_page, sub_page):
+        edge = pydot.Edge(parent_page.title, sub_page.title)
+        self.g.add_edge(edge)
+
+    def write_out(self):
+        self.g.write_png(self.search_term+'.png')
 
 
-# use the wikipedia library to easily interact with wikipedia pages
-wiki_wiki = wikipediaapi.Wikipedia('en')
+def main():
+    input_page = input("Page to search: ")
 
-input_page = input("Page to search: ")
+    # create a new graph for this search term
+    g = Graph(input_page, 20)
+    g.create_links()
+    g.write_out()
 
-# lets start by statically setting our starter page to 'dog'
-start_page = wiki_wiki.page(input_page)
-if start_page.exists() != True:
-    print("Page: %s does not exist" % input_page)
-    exit
-
-g = pydot.Dot(graph_type="digraph")
-
-node = pydot.Node(start_page.title, style="filled", fillcolor="red")
-# add the starter page as a node
-g.add_node(node)
-
-link_pages = get_links(start_page)
-# now get the first five links off of the links pages
-for link_page in link_pages:
-    node = pydot.Node(link_page.title, style="filled", fillcolor="red")
-    g.add_node(node)
-    edge = pydot.Edge(start_page.title, link_page.title)
-    g.add_edge(edge)
-    
-    # now add the sub-pages of these links
-    sub_link_pages = get_links(link_page)
-    for s_page in sub_link_pages:
-        # create a node for each sub page
-        node = pydot.Node(s_page.title, style="filled", fillcolor="red")
-        g.add_node(node)
-        edge = pydot.Edge(link_page.title, s_page.title)
-        g.add_edge(edge)
-    
-
-g.write_png('example1.png')
-
+main()
     
